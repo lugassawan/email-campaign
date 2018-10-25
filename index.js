992c2a95828ac4bbbb321eb34d4c53ac57cf75ad
Lugas Septiawan <lugassawan@gmail.com>
2018-10-25 22:28:30 +0700

    finished auth flow

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+//local
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'Hellow world' });
-});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 app.listen(PORT);
